14b99a4f9447a07210a5a9b93e6ce467f3092a8c
joeydenepstudent <jdenepstudent@roc-dev.com>
2020-09-08 11:52:56 +0200

    Added Github demo.

---
+++ src/h02/Github.java
@@ -0,0 +1,14 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Github extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+    }
+
+}
---
+++ src/h02/Test.java
@@ -0,0 +1,15 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Test extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Hallo hallo", 50, 50);
+    }
+}
